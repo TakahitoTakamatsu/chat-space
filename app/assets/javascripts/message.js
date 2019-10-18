dbd62f39a46f095b4deb8c5138418dbd30c5d958
TakahitoTakamatsu <takamatsutakahito@gmail.com>
2019-10-18 17:10:46 +0900

    addevent

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,44 @@
 $(function() {
-  $('.form').on('submit', function(e) {
+  function buildHTML(message){
+    var html = `<div class="messages__datamessage__id">
+                  <div class="messages__upper__info">
+                    <p class="messages__upper__info__talker">
+                      ${message.name}
+                    </p>
+                    <p class="messages__upper__info__date">
+                      ${message.created_at}
+                    </p>
+                  </div>
+                    <p class="messages__text">
+                    ${message.content?
+                      `<p class="lower-message__content">${message.content}</p>`:``}
+                    ${message.image?
+                      `<img src = ${message.image} class = "lower-message__image">`:``}
+                    </p>
+                </div>`
+    return html;
+  }
+
+
+
+
+  $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    todo = $('.form__message').val();
-    console.log(todo);
-    $("#submit").prop("disabled", false);
-  });
+    var formData = new FormData(this);
+    var href = $(this).attr('action');
+    $.ajax({
+      //jbuilderへ行った後にjsonを持ってcontrollerへ
+      url: href,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.message').append(html)
+      $("#submit").prop("disabled", false);
+    })
+  })
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,13 +4,17 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    @menbers = group_menber(@group)
   end
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
       
+      respond_to do |format|
+        format.html 
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
@@ -26,5 +30,13 @@ class MessagesController < ApplicationController
 
   def set_group
     @group = Group.find(params[:group_id])
+  end
+
+  def group_menber(group)
+    menbers = []
+    group.users.each do |menber|
+      menbers << menber.name
+    end
+    menbers = menbers.join(" ")
   end
 end
