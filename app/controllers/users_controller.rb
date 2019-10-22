29839fa28074e8ed657f1431f8c06eb1e6590214
TakahitoTakamatsu <takamatsutakahito@gmail.com>
2019-10-22 20:34:07 +0900

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,29 @@
 class UsersController < ApplicationController
-  def index
-    respond_to do |format|
-      format.html
-      format.json
-  end
-  
+
   def edit
   end
-  
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
-    else
+      else
       render :edit
     end
   end
 
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
+      respond_to do |format|
+      format.json
+      format.html
+      
+      end
+      
+  end
+
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
